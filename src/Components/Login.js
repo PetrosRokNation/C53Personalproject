3d5d665e4c75eb855725529133f02d1be3bc39a6
PetrosRokNation <phong84@gmail.com>
2018-07-23 19:36:18 -0700

    register complete

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,96 @@
 import React from 'react';
 
-class Login extends React.Component{
+class Login extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            userName: "",
+            password: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onSubmit_Sucess = this.onSubmit_Sucess.bind(this);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                This is the login Page
+                <div className="main-login main-center">
+                    <form className="" method="post" action="#">
+
+
+
+                        <div className="form-group">
+                            <label htmlFor="email" className="cols-sm-2 control-label">Your Email</label>
+                            <div className="cols-sm-10">
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"></i></span>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="email"
+                                        id="email"
+                                        placeholder="Enter your Email"
+                                        value={this.state.email}
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="form-group">
+                            <label htmlFor="password" className="cols-sm-2 control-label">Password</label>
+                            <div className="cols-sm-10">
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        name="password"
+                                        id="password"
+                                        placeholder="Enter your Password"
+                                        value={this.state.password}
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="confirm" className="cols-sm-2 control-label">Confirm Password</label>
+                            <div className="cols-sm-10">
+                                <div className="input-group">
+                                    <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        name="confirm"
+                                        id="confirm"
+                                        placeholder="Confirm your Password"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="form-group ">
+                            <button
+                                type="button"
+                                id="button"
+                                className="btn btn-primary btn-lg btn-block login-button"
+                                onClick={this.onSubmit}
+                            >
+                                Login
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         );
     }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,49 @@
 import React from 'react';
-import './Register.css'
+import './Register.css';
+import RegisterApi from '../api/RegisterApi';
 
 class Register extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            userName: "",
+            password: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onSubmit_Sucess = this.onSubmit_Sucess.bind(this);
+    }
+
+    handleChange(e) {
+        let key = e.target.name;
+        let val = e.target.value;
+        this.setState({
+            ...this.state,
+            [key]: val,
+        })
+    }
+
+    onSubmit(e) {
+        const registerData = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            userName: this.state.userName,
+            password: this.state.password
+        }
+        RegisterApi.Register(registerData, this.onSubmit_Sucess, this.onError);
+    }
+
+    onSubmit_Sucess(resp){
+        console.log(resp);
+    }
+    
+    onError(err) {
+        console.log(err)
     }
 
     render() {
@@ -20,7 +60,15 @@ class Register extends React.Component {
                                     <div className="cols-sm-10">
                                         <div className="input-group">
                                             <span className="input-group-addon"><i className="fa fa-user fa" aria-hidden="true"></i></span>
-                                            <input type="text" className="form-control" name="firstName" id="firstName" placeholder="Enter your first Name" />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="firstName"
+                                                id="firstName"
+                                                placeholder="Enter your first Name"
+                                                value={this.state.firstName}
+                                                onChange={this.handleChange}
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -30,7 +78,15 @@ class Register extends React.Component {
                                     <div className="cols-sm-10">
                                         <div className="input-group">
                                             <span className="input-group-addon"><i className="fa fa-user fa" aria-hidden="true"></i></span>
-                                            <input type="text" className="form-control" name="lastName" id="lastName" placeholder="Enter your Name" />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="lastName"
+                                                id="lastName"
+                                                placeholder="Enter your Name"
+                                                value={this.state.lastName}
+                                                onChange={this.handleChange}
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -40,7 +96,15 @@ class Register extends React.Component {
                                     <div className="cols-sm-10">
                                         <div className="input-group">
                                             <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"></i></span>
-                                            <input type="text" className="form-control" name="email" id="email" placeholder="Enter your Email" />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="email"
+                                                id="email"
+                                                placeholder="Enter your Email"
+                                                value={this.state.email}
+                                                onChange={this.handleChange}
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -50,7 +114,15 @@ class Register extends React.Component {
                                     <div className="cols-sm-10">
                                         <div className="input-group">
                                             <span className="input-group-addon"><i className="fa fa-users fa" aria-hidden="true"></i></span>
-                                            <input type="text" className="form-control" name="username" id="username" placeholder="Enter your Username" />
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                name="userName"
+                                                id="userName"
+                                                placeholder="Enter your Username"
+                                                value={this.state.userName}
+                                                onChange={this.handleChange}
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -60,7 +132,15 @@ class Register extends React.Component {
                                     <div className="cols-sm-10">
                                         <div className="input-group">
                                             <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                            <input type="password" className="form-control" name="password" id="password" placeholder="Enter your Password" />
+                                            <input
+                                                type="password"
+                                                className="form-control"
+                                                name="password"
+                                                id="password"
+                                                placeholder="Enter your Password"
+                                                value={this.state.password}
+                                                onChange={this.handleChange}
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -70,13 +150,26 @@ class Register extends React.Component {
                                     <div className="cols-sm-10">
                                         <div className="input-group">
                                             <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                            <input type="password" className="form-control" name="confirm" id="confirm" placeholder="Confirm your Password" />
+                                            <input
+                                                type="password"
+                                                className="form-control"
+                                                name="confirm"
+                                                id="confirm"
+                                                placeholder="Confirm your Password"
+                                            />
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="form-group ">
-                                    <a href="/login" target="_blank" type="button" id="button" className="btn btn-primary btn-lg btn-block login-button">Register</a>
+                                    <button
+                                        type="button"
+                                        id="button"
+                                        className="btn btn-primary btn-lg btn-block login-button"
+                                        onClick={this.onSubmit}
+                                    >
+                                        Register
+                                    </button>
                                 </div>
 
                             </form>
